94cdff843e4d6fffc11105d3676492856583f4da
SkorihV <skorih.vitalii@gmail.com>
2022-04-01 17:30:28 +0700

    1:00:00

---
+++ src/components/PostForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <my-input
+        v-model="post.title"
+        placeholder="Название"
+    ></my-input>
+    <my-input
+        v-model="post.body"
+        placeholder="Описание"
+    ></my-input>
+    <my-button
+        style="margin-top: 10px"
+        class="btn"
+        @click="cretePost"
+    >
+      Создать
+    </my-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      },
+
+    }
+  },
+  methods: {
+    cretePost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: '',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Список пользователей</h3>
+  </div>
+  <post-item
+      v-for="post in postss"
+      class="post"
+      :post="post"
+  ></post-item>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components: {PostItem},
+  props: {
+    postss: {
+      type: Array,
+      require: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
